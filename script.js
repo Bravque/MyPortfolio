9a47445556cd4273e2a10d1d6ec7a95aff87fe15
Bravin <bravinowino008@gmail.com>
2024-07-14 23:03:26 +0300

    minor changes

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*==toggle icon navbar==*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+};
+
 /*==scroll section active link==*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -17,4 +27,39 @@ window.onscroll = () => {
 
         };
     });
+    /*==sticky navbar==*/
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==remove toggle icon==*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+
+    /*==scroll reveal==*/
+    ScrollReveal({ 
+        reset: true,
+        distance: '80px',
+        duration: 1000,
+        delay: 200
+
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .project, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*==============typed js===========*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer', 'Frontend Developer', 'UI/UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 2000
+});
+
